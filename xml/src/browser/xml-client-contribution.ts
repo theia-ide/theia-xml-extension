07f8d491b972c8a761175bec65972a6990dd6e9b
Dennis Huebner <dennis.huebner@gmail.com>
2018-12-06 14:01:45 +0100

    Changing preferences doesn't affect the server side #1

--- xml/src/browser/xml-client-contribution.ts
+++ xml/src/browser/xml-client-contribution.ts
@@ -15,7 +15,7 @@
  ********************************************************************************/
 
 import { injectable, inject } from "inversify";
-import { BaseLanguageClientContribution, Workspace, Languages, LanguageClientFactory, ILanguageClient } from '@theia/languages/lib/browser';
+import { BaseLanguageClientContribution, Workspace, Languages, LanguageClientFactory, ILanguageClient, DidChangeConfigurationNotification } from '@theia/languages/lib/browser';
 import { XML_LANGUAGE_ID, XML_LANGUAGE_NAME } from '../common';
 import { ResourceProvider } from "@theia/core";
 import URI from "@theia/core/lib/common/uri";
@@ -25,7 +25,10 @@ export class XMLClientContribution extends BaseLanguageClientContribution {
 
     readonly id = XML_LANGUAGE_ID;
     readonly name = XML_LANGUAGE_NAME;
-    get configurationSection() { return 'xml'; }
+
+    get configurationSection() {
+        return ['xml'];
+    }
 
     constructor(
         @inject(Workspace) protected readonly workspace: Workspace,
@@ -53,5 +56,42 @@ export class XMLClientContribution extends BaseLanguageClientContribution {
         });
         super.onReady(languageClient);
     }
+    createOptions() {
+        const options = super.createOptions();
+        options.middleware =  {
+            workspace: {
+                didChangeConfiguration: () => {
+                    this.languageClient.then(client => {
+                        client.sendNotification(DidChangeConfigurationNotification.type, { settings: this.getSettings() });
+                    });
+                }
+            }
+        }
+        return options;
+    }
 
+    getSettings(): JSON {
+        const configXML = this.workspace.configurations!.getConfiguration('xml');
+        let settings: JSON;
+        if (!configXML) {
+          const defaultValue = {
+            trace: {
+              server: 'verbose'
+            },
+            logs: {
+              client: true
+            },
+            format: {
+              enabled: true,
+              splitAttributes: false
+            }
+          }
+          const x = JSON.stringify(defaultValue);
+          settings = JSON.parse(x);
+        } else {
+          const x = JSON.stringify(configXML);
+          settings = JSON.parse(x);
+        }
+        return settings;
+    }
 }
